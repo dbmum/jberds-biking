e715210e9101174cdd8d9985a8a3cc774ac9c8c1
Joel Jossie <joeljossie@gmail.com>
2023-06-01 15:30:07 -0600

    working on environment setup still

--- joel.py
+++ joel.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 import tensorflow as tf
-
 from tensorflow import keras
 from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
 
 print(tf.__version__)
 
@@ -18,3 +18,8 @@ np.set_printoptions(precision=3, suppress=True)
 bike_rentals = pd.read_csv("https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/bikes.csv")
 bike_rentals_holdout = pd.read_csv("https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/bikes_december.csv")
 
+# Define X/Y
+bike_rentals["total_rentals"] = bike_rentals.casual + bike_rentals.registered
+
+# Set aside the test set immediately
+bike_rentals = train_test_split()
